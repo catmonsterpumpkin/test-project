3c8b67d71ad44f102a73531b5bd5df16589218ee
Felix Sharpe-Lewis <fsharpelewis@gmail.com>
2024-11-10 12:55:21 -0500

    Make Jupiter bounce around.

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+const ROTATION_SPEED = 1
+
+var direction: Vector2 = Vector2(100, 100)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +11,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_RIGHT):
-		position.x += 10 * delta
+	rotation += delta * ROTATION_SPEED
+	position += delta * direction
+	if position.x > get_viewport_rect().size.x or position.x < 0:
+		direction.x = -direction.x
+	if position.y > get_viewport_rect().size.y or position.y < 0:
+		direction.y = -direction.y
+	
+	#if Input.is_key_pressed(KEY_RIGHT):
+	#	position.x += 10 * delta
